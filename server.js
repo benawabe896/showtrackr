e32417e1a00cc7505ffbdcecf35fbc5859200a74
ben-powers <ben.powers@am.sony.com>
2014-07-04 09:31:28 -0700

    Good point to test

--- server.js
+++ server.js
@@ -13,6 +13,6 @@ app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(app.get('port'), function(){
+app.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'));
 });
